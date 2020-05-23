492278aa488d247496b913ea671e3848b14668e7
Nguyen Nguyen <nguyennguyen6395@gmail.com>
2020-05-23 20:02:49 +1200

    done w startup file

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,9 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using LocalizationServer.Data;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
 
 namespace LocalizationServer
 {
@@ -39,15 +42,43 @@ namespace LocalizationServer
 				.AddDataAnnotationsLocalization();
 
 			services.AddScoped<LanguageActionFilter>();
+			services.Configure<RequestLocalizationOptions>(
+			options =>
+			{
+				var supportedCultures = new List<CultureInfo>
+					{
+						new CultureInfo("en-US"),
+						new CultureInfo("de-CH"),
+						new CultureInfo("fr-CH"),
+						new CultureInfo("it-CH")
+					};
+
+				options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
+				options.SupportedCultures = supportedCultures;
+				options.SupportedUICultures = supportedCultures;
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceCollection loggerFactory)
 		{
+			loggerFactory.AddLogging(opt => {
+				opt.AddConsole();
+			});
+
+			loggerFactory.AddLogging(opt => {
+				opt.AddDebug();
+			});
+
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
 			}
+
+			var locOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+			app.UseRequestLocalization(locOptions.Value);
+
+			app.UseStaticFiles();
 
 			app.UseHttpsRedirection();
 
