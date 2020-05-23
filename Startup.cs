eaf42da2549cb69227b46a97b608d15da497d10d
Nguyen Nguyen <nguyennguyen6395@gmail.com>
2020-05-23 19:44:44 +1200

    add LanguageActionFilter.cs

--- Startup.cs
+++ Startup.cs
@@ -37,6 +37,8 @@ namespace LocalizationServer
 			services.AddMvc()
 				.AddViewLocalization()
 				.AddDataAnnotationsLocalization();
+
+			services.AddScoped<LanguageActionFilter>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
