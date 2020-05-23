914c914c17e520fab36051f3ecebe00bf2b1ddc2
Nguyen Nguyen <nguyennguyen6395@gmail.com>
2020-05-23 21:24:32 +1200

    reset to good state

--- Startup.cs
+++ Startup.cs
@@ -12,9 +12,6 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using LocalizationServer.Data;
-using System.Globalization;
-using Microsoft.AspNetCore.Localization;
-using Microsoft.Extensions.Options;
 
 namespace LocalizationServer
 {
@@ -34,51 +31,15 @@ namespace LocalizationServer
 
 			var connection = Configuration.GetConnectionString("LocalizationServerContext");
 			services.AddDbContext<LocalizationServerContext>(options => options.UseSqlServer(connection));
-
-			services.AddSqlLocalization();
-			
-			services.AddMvc()
-				.AddViewLocalization()
-				.AddDataAnnotationsLocalization();
-
-			services.AddScoped<LanguageActionFilter>();
-			services.Configure<RequestLocalizationOptions>(
-			options =>
-			{
-				var supportedCultures = new List<CultureInfo>
-					{
-						new CultureInfo("en-US"),
-						new CultureInfo("de-CH"),
-						new CultureInfo("fr-CH"),
-						new CultureInfo("it-CH")
-					};
-
-				options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
-				options.SupportedCultures = supportedCultures;
-				options.SupportedUICultures = supportedCultures;
-			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceCollection loggerFactory)
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			loggerFactory.AddLogging(opt => {
-				opt.AddConsole();
-			});
-
-			loggerFactory.AddLogging(opt => {
-				opt.AddDebug();
-			});
-
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
 			}
-
-			var locOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
-			app.UseRequestLocalization(locOptions.Value);
-
-			app.UseStaticFiles();
 
 			app.UseHttpsRedirection();
 
